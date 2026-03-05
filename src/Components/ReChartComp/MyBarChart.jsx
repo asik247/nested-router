27d4b0cf5e0ed881c54ae440d6b131ca4fa4ea30
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-05 07:21:27 +0600

    done

--- src/Components/ReChartComp/MyBarChart.jsx
+++ src/Components/ReChartComp/MyBarChart.jsx
@@ -1,45 +1,39 @@
 import { use } from "react";
+import { 
+  Bar, 
+  BarChart, 
+  CartesianGrid, 
+  Tooltip, 
+  XAxis, 
+  YAxis 
+} from "recharts";
 
+const MyBarChart = ({ promiseAxios }) => {
 
-const MyBarChart = ({promiseAxios}) => {
-    // console.log(promiseAxios);
     const promiseData = use(promiseAxios);
-    console.log(promiseData);
-    const myAllData = promiseData.data
-   
-    
+    const students = promiseData.data;
+
+    // nested array flatten করে এক লেভেলে নিয়ে আসলাম
+    const chartData = students.flatMap(student =>
+        student.subjects.map(sub => ({
+            student: student.name,
+            book: sub.subject,
+            mark: sub.obtained_marks
+        }))
+    );
+
     return (
-        <div>
-            {/* <h1>bar chart here</h1> */}
-            {
-                myAllData.map(singleData=>{
-                    const chartData={
-                        sub:singleData.subject,
-                        // Bangla:singleData.subject,
-                        // Mathematics:singleData.subject,
-                        // Science:singleData.subject,
-                        mark:singleData.full_marks,
-
-
-
-                    }
-                })
-            }
+       <div>
+            <h2>Student Marks Chart</h2>
+            <BarChart width={700} height={400} data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="book" />   {/* subject name */}
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="mark" />
+            </BarChart>
         </div>
     );
 };
-/**
- * 
-    "student_id": "STU1001",
-    "name": "Rahim Ahmed",
-    "class": "10",
-    "section": "A",
-    "roll_number": 1,
-    "subjects": [
-      { "subject": "English", "full_marks": 100, "obtained_marks": 85 },
-      { "subject": "Bangla", "full_marks": 100, "obtained_marks": 78 },
-      { "subject": "Mathematics", "full_marks": 100, "obtained_marks": 92 },
-      { "subject": "Science", "full_marks": 100, "obtained_marks": 88 }
- */
 
 export default MyBarChart;
