2ed5bcd844bcaac5ca04c01420f9f9e7040f66db
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-05 06:49:26 +0600

    chart data leveling and showing chart display

--- src/Components/ReChartComp/MyBarChart.jsx
+++ src/Components/ReChartComp/MyBarChart.jsx
@@ -12,10 +12,34 @@ const MyBarChart = ({promiseAxios}) => {
         <div>
             {/* <h1>bar chart here</h1> */}
             {
-                myAllData.map(singleData=>console.log(singleData))
+                myAllData.map(singleData=>{
+                    const chartData={
+                        sub:singleData.subject,
+                        // Bangla:singleData.subject,
+                        // Mathematics:singleData.subject,
+                        // Science:singleData.subject,
+                        mark:singleData.full_marks,
+
+
+
+                    }
+                })
             }
         </div>
     );
 };
+/**
+ * 
+    "student_id": "STU1001",
+    "name": "Rahim Ahmed",
+    "class": "10",
+    "section": "A",
+    "roll_number": 1,
+    "subjects": [
+      { "subject": "English", "full_marks": 100, "obtained_marks": 85 },
+      { "subject": "Bangla", "full_marks": 100, "obtained_marks": 78 },
+      { "subject": "Mathematics", "full_marks": 100, "obtained_marks": 92 },
+      { "subject": "Science", "full_marks": 100, "obtained_marks": 88 }
+ */
 
 export default MyBarChart;
