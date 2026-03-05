48632a8f621ca36cd5271954ce9f5fd80d7cd957
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-05 06:06:42 +0600

    nested router code execuie

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>hi nested router and recharts</h1>
+     
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Components/Route/Root.jsx'
+import About from './Components/About/About.jsx'
+
+// Nested router code start here;
+const router = createBrowserRouter([
+  {
+    path:"/",
+    Component: Root,
+    children:[
+      {index:true,Component:About}
+    ]
+  }
+])
+// Nested router code end here;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
